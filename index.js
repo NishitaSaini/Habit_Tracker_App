4cddcb70ffa1e175c122a4008ea553629926ecda
NishitaSaini <saininishi1122@gmail.com>
2024-04-04 16:17:28 +0530

    commit

--- index.js
+++ index.js
@@ -5,19 +5,17 @@ import path from "path";
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import habitRoutes from "./src/routes/habit.route.js";
-import mongoose from "mongoose";
 
-dotenv.config(); // Load environment variables from .env file
+
+ // Load environment variables from .env file
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
-const port = process.env.PORT || 5000;
 
-mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true})
-.then( ()=> console.log("Connected to MongoDB"))
-.catch(err => console.error('Failed to connect to MongoDB: ', err));
+
+
 
 
 // app.use(bodyParser.urlencoded({ extended: true }));
@@ -29,6 +27,4 @@ app.use(express.static(path.join(__dirname, 'src', 'public')));
 
 app.use('/', habitRoutes);
 
-app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
-});
+export default app;
---
+++ server.js
@@ -0,0 +1,13 @@
+import app from "./index.js";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+.then( ()=> console.log("Connected to MongoDB"))
+.catch(err => console.error('Failed to connect to MongoDB: ', err));
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
