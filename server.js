c7942c951847b49a778f6d10c89f0789d8c8986f
NishitaSaini <saininishi1122@gmail.com>
2024-04-07 15:22:09 +0530

    commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
-mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true, serverSelectionTimeoutMS: 30000})
 .then( ()=> console.log("Connected to MongoDB"))
 .catch(err => console.error('Failed to connect to MongoDB: ', err));
 
